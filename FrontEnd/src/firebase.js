5220bfa6c03c033317d79ffebd17f8d92a2fd380
arun375797 <arun375797@gmail.com>
2024-11-02 08:07:05 +0530

    the last commit

--- FrontEnd/src/firebase.js
+++ FrontEnd/src/firebase.js
@@ -7,11 +7,11 @@ import { initializeApp } from "firebase/app";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "react-auth-project-week-9.firebaseapp.com",
-  projectId: "react-auth-project-week-9",
-  storageBucket: "react-auth-project-week-9.appspot.com",
-  messagingSenderId: "159205083915",
-  appId: "1:159205083915:web:94a30e4e03791f1cd4f00e"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
